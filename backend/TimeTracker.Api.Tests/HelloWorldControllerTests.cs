8b492b703dcc12ed83dab2ba3d5275cd4d9f4979
Hans Buys <hansbuys@Hanss-MacBook-Pro.local>
2018-10-03 12:03:25 -0400

    initial commit for the .net core backend, working unit test setup

---
+++ backend/TimeTracker.Api.Tests/HelloWorldControllerTests.cs
@@ -0,0 +1,33 @@
+using System;
+using Xunit;
+using TimeTracker.Api;
+using Microsoft.Extensions.DependencyInjection;
+using TimeTracker.Api.Controllers;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace TimeTracker.Api.Tests
+{
+    public class HelloWorldControllerTests
+    {
+        protected HelloWorldController GetSut() {
+            IServiceCollection service = new ServiceCollection();
+
+            service.AddTransient<HelloWorldController>();
+
+            var serviceProvider = service.BuildServiceProvider();
+
+            return serviceProvider.GetService<HelloWorldController>();
+        }
+
+        [Fact]
+        public async Task CanSayHello()
+        {
+            var sut = GetSut();
+
+            var result = await sut.SayHello();
+
+            result.Value.Should().Be("Hello from the backend");
+        }
+    }
+}
