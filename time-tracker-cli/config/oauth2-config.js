018f65b8e012a97012b6eb6dd48e53b54c855168
feysaw <aaronfeys@gmail.com>
2018-11-01 15:03:55 -0400

    WIP on the config command. Creating authentication method using Google auth.

---
+++ time-tracker-cli/config/oauth2-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  client_id: '746393280804-g6h55ss80310qla31fbe2e9hr3hl4512.apps.googleusercontent.com',
+  redirect_uri_path: '/oauth2callback',
+  scope: 'https://www.googleapis.com/auth/plus.me'
+};
