d275df8d208e285b80f4a6a5b832dc0e54a17160
James Reategui <james@integral.io>
2019-01-29 18:24:22 -0500

    basic help printout

--- time-tracker-webapi/src/TimeTracker.Api/Controllers/SlackSlashCommandController.cs
+++ time-tracker-webapi/src/TimeTracker.Api/Controllers/SlackSlashCommandController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TimeTracker.Api.Infrastructure;
@@ -25,13 +26,14 @@ namespace TimeTracker.Api.Controllers
         public async Task<ActionResult<SlackMessage>> HandleCommand([FromForm]SlashCommandPayload slashCommandPayload)
         {
             string option = slashCommandPayload.text.Split(' ').FirstOrDefault();
+            SlackMessageOptions optionEnum;
+            SlackMessageOptions.TryParse(option, true, out optionEnum);
+            
             var userSevice = new UserService(_dbContext);
             SlackMessage message;
             
             var user = await userSevice.FindOrCreateSlackUser(slashCommandPayload.user_id, slashCommandPayload.user_name);
             var timeEntryService = new TimeEntryService(user.UserId, _dbContext);
-            SlackMessageOptions optionEnum;
-            SlackMessageOptions.TryParse(option, true, out optionEnum);
                 
             switch (optionEnum)
             {
@@ -82,11 +84,20 @@ namespace TimeTracker.Api.Controllers
                     message = BuildMessage($"Deleted {hoursDeleted:F1} hours", "success");
                     return Ok(message);
                 }
-                // TODO - implement default/help
-                /*default:
+                default:
                 {
+                    StringBuilder sb = new StringBuilder();
+                    sb.AppendLine("*/hours* record <projectName> <hours> _Will use today date by default_");
+                    sb.AppendLine("*/hours* record <projectName> <hours> jan-21 _sets date to january 21 current year_");
+                    sb.AppendLine("*/hours* record <projectName> <hours> wfh _wfh option marks as worked from home_");
+                    sb.AppendLine("*/hours* record sick <hours> <optional: date> _marks sick hours_");
+                    sb.AppendLine("*/hours* record vacation <hours> <optional: date> _marks vacation hours_");
+                    sb.AppendLine("*/hours* report <optional: date> _generate report of hours_");
+                    sb.AppendLine("*/hours* delete <optional: date> _delete all hours for the date_");
                     
-                }*/
+                    message = BuildMessage(sb.ToString(), "success");
+                    return Ok(message);
+                }
             }
 
             return Ok("unsupported option");
