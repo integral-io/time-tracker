8d9d204c31d6e4d045f28afee79a088d0a1d8fe8
James Reategui <james@integral.io>
2019-01-31 11:43:05 -0500

    add help for nonbill

--- time-tracker-webapi/src/TimeTracker.Api/Controllers/SlackSlashCommandController.cs
+++ time-tracker-webapi/src/TimeTracker.Api/Controllers/SlackSlashCommandController.cs
@@ -89,6 +89,7 @@ namespace TimeTracker.Api.Controllers
                     sb.AppendLine("*/hours* record <projectName> <hours> _Will use today date by default_");
                     sb.AppendLine("*/hours* record <projectName> <hours> jan-21 _sets date to january 21 current year_");
                     sb.AppendLine("*/hours* record <projectName> <hours> wfh _wfh option marks as worked from home_");
+                    sb.AppendLine("*/hours* record nonbill <hours> <optional: date> \"non billable reason\" _non billable hour for a given reason, ie PDA_");
                     sb.AppendLine("*/hours* record sick <hours> <optional: date> _marks sick hours_");
                     sb.AppendLine("*/hours* record vacation <hours> <optional: date> _marks vacation hours_");
                     sb.AppendLine("*/hours* report <optional: date> _generate report of hours_");
