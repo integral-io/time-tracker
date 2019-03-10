567f9ec409a8625932bec09744211150d34f10b7
James Reategui <james@integral.io>
2019-03-09 22:40:33 -0500

    wip razor page

---
+++ time-tracker-webapi/src/TimeTracker.Api/Controllers/AdminReportsController.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TimeTracker.Data;
+using TimeTracker.Library.Models;
+using TimeTracker.Library.Models.Admin;
+using TimeTracker.Library.Services;
+
+namespace TimeTracker.Api.Controllers
+{
+    [Route("admin/reports")]
+    public class AdminReportsController : Controller
+    {
+        private readonly TimeTrackerDbContext _dbContext;
+
+        public AdminReportsController(TimeTrackerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /* anonymous until figure out auth with google */
+        [HttpGet(""), AllowAnonymous, ProducesResponseType(typeof(IEnumerable<UserReport>), 200)]
+        public async Task<IActionResult> AllUsersReport()
+        {
+            AdminReportService adminReportService = new AdminReportService(_dbContext);
+            var items = await adminReportService.GetAllUsersReport();
+            return View(model: items);
+        }
+    }
+}
--- time-tracker-webapi/src/TimeTracker.Api/Controllers/InvoiceController.cs
+++ time-tracker-webapi/src/TimeTracker.Api/Controllers/InvoiceController.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TimeTracker.Api.Models;
+using TimeTracker.Data;
 using TimeTracker.Library.Models;
 
 namespace TimeTracker.Api.Controllers
@@ -9,17 +10,17 @@ namespace TimeTracker.Api.Controllers
     [Route("invoice")]
     public class InvoiceController : Controller
     {
+        private readonly TimeTrackerDbContext _dbContext;
+
+        public InvoiceController(TimeTrackerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         [HttpGet("month/{monthDate}"), AllowAnonymous] // anon temp
         public async Task<IActionResult> Month(string monthDate)
         {
             return View(model: new InvoiceDto());
-        }
-        
-        
-        [HttpGet("adminreport"), AllowAnonymous] // anon temp
-        public async Task<IActionResult> AdminTimeOffReport()
-        {
-            return View(model: new AllTimeOff());
         }
         
         
