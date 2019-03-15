dd4faa3eb9ad2f32e901f1bf24ec159d0df5497f
James Reategui <james@integral.io>
2019-03-15 11:24:31 -0400

    address some more PR feedback in testing

--- time-tracker-webapi/test/TimeTracker.Library.Test/Services/AdminReportServiceTest.cs
+++ time-tracker-webapi/test/TimeTracker.Library.Test/Services/AdminReportServiceTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -14,42 +15,61 @@ namespace TimeTracker.Library.Test.Services
     public class AdminReportServiceTest
     {
         [Fact]
-        public async Task GetAllUsersReport_returnsExpectedUsers_withHours()
+        public async Task GetAllUsersReport_includesExpectedHours()
         {
             using (var context = new TimeTrackerDbContext(TestHelpers.BuildInMemoryDatabaseOptions("adminReport")))
             {
                 TestHelpers.AddClientAndProject(context);
                 var testUsers = TestHelpers.AddTestUsers(context);
-
-                TimeEntryService timeEntryService = new TimeEntryService(testUsers.First().UserId, context);
-                DateTime currentDate = DateTime.UtcNow.Date;
-                DateTime date = new DateTime(currentDate.Year, 2, 15);
-
-                await timeEntryService.CreateBillableTimeEntry(date, 8, 1, 1);
-                await timeEntryService.CreateBillableTimeEntry(date.AddDays(-1), 4, 1, 1);
-                await timeEntryService.CreateNonBillableTimeEntry(date.AddDays(-3), 4, "dr visit",
-                    TimeEntryTypeEnum.Sick);
-                await timeEntryService.CreateNonBillableTimeEntry(date.AddDays(-2), 2, "dr visit",
-                    TimeEntryTypeEnum.Sick);
-                await timeEntryService.CreateNonBillableTimeEntry(date.AddDays(-5), 8, null,
-                    TimeEntryTypeEnum.Vacation);
-                await timeEntryService.CreateNonBillableTimeEntry(date.AddDays(-6), 6, null,
-                    TimeEntryTypeEnum.Vacation);
-                await timeEntryService.CreateNonBillableTimeEntry(date.AddDays(-7), 4, "pda",
-                    TimeEntryTypeEnum.NonBillable);
-                await timeEntryService.CreateNonBillableTimeEntry(date.AddDays(-8), 2, "ipa",
-                    TimeEntryTypeEnum.NonBillable);
-
+                await PopulateTimeEntries(context, testUsers);
+                
                 AdminReportService sut = new AdminReportService(context);
                 var report = await sut.GetAllUsersReport();
-                report.Count.Should().Be(2);
-                report.Any(x => x.SlackUserName == testUsers.First().UserName).Should().BeTrue();
-                report.Any(x => x.SlackUserName == testUsers.Last().UserName).Should().BeTrue();
+
                 report.First().BillableHoursYtd.Should().Be(12);
                 report.First().SickHoursYtd.Should().Be(6);
                 report.First().VacationHoursYtd.Should().Be(14);
                 report.First().OtherNonBillableYtd.Should().Be(6);
             }
+        }
+        
+        [Fact]
+        public async Task GetAllUsersReport_includesExpectedUsers()
+        {
+            using (var context = new TimeTrackerDbContext(TestHelpers.BuildInMemoryDatabaseOptions("adminReport")))
+            {
+                TestHelpers.AddClientAndProject(context);
+                var testUsers = TestHelpers.AddTestUsers(context);
+                await PopulateTimeEntries(context, testUsers);
+                
+                AdminReportService sut = new AdminReportService(context);
+                var report = await sut.GetAllUsersReport();
+                report.Count.Should().Be(2);
+
+                report.Where(x => x.SlackUserName == testUsers.First().UserName).Should().NotBeEmpty();
+                report.Where(x => x.SlackUserName == testUsers.Last().UserName).Should().NotBeEmpty();
+            }
+        }
+
+        private async Task PopulateTimeEntries(TimeTrackerDbContext context, IEnumerable<User> testUsers)
+        {
+            TimeEntryService timeEntryService = new TimeEntryService(testUsers.First().UserId, context);
+            DateTime date = new DateTime(DateTime.UtcNow.Date.Year, 2, 15);
+
+            await timeEntryService.CreateBillableTimeEntry(date, 8, 1, 1);
+            await timeEntryService.CreateBillableTimeEntry(date.AddDays(-1), 4, 1, 1);
+            await timeEntryService.CreateNonBillableTimeEntry(date.AddDays(-3), 4, "dr visit",
+                TimeEntryTypeEnum.Sick);
+            await timeEntryService.CreateNonBillableTimeEntry(date.AddDays(-2), 2, "dr visit",
+                TimeEntryTypeEnum.Sick);
+            await timeEntryService.CreateNonBillableTimeEntry(date.AddDays(-5), 8, null,
+                TimeEntryTypeEnum.Vacation);
+            await timeEntryService.CreateNonBillableTimeEntry(date.AddDays(-6), 6, null,
+                TimeEntryTypeEnum.Vacation);
+            await timeEntryService.CreateNonBillableTimeEntry(date.AddDays(-7), 4, "pda",
+                TimeEntryTypeEnum.NonBillable);
+            await timeEntryService.CreateNonBillableTimeEntry(date.AddDays(-8), 2, "ipa",
+                TimeEntryTypeEnum.NonBillable);
         }
     }
 }
