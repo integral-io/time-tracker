19d449f9ae4ff27d765aeed8fbdf1a069e8ad946
James Reategui <james@integral.io>
2019-03-26 16:41:22 -0400

    Add report with date range.

--- src/TimeTracker.Library/Services/AdminReportService.cs
+++ src/TimeTracker.Library/Services/AdminReportService.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using TimeTracker.Data;
 using System.Linq;
@@ -14,6 +16,7 @@ namespace TimeTracker.Library.Services
     public class AdminReportService
     {
         private readonly TimeTrackerDbContext db;
+        private const int _payPeriodLength = 14;
 
         public AdminReportService(in TimeTrackerDbContext db)
         {
@@ -32,6 +35,41 @@ namespace TimeTracker.Library.Services
                     OtherNonBillableYtd = u.TimeEntries.Where(x=>x.TimeEntryType == TimeEntryTypeEnum.NonBillable).Sum(x=>x.Hours),
                     SickHoursYtd = u.TimeEntries.Where(x=>x.TimeEntryType == TimeEntryTypeEnum.Sick).Sum(x=>x.Hours),
                     VacationHoursYtd = u.TimeEntries.Where(x=>x.TimeEntryType == TimeEntryTypeEnum.Vacation).Sum(x=>x.Hours)
+                };
+            return (await query.ToListAsync()).ToImmutableList();
+        }
+
+        /// <summary>
+        /// This function returns an "Hours Report" filtered by date.
+        /// </summary>
+        /// <param name="start">Start date for query, inclusive.</param>
+        /// <param name="end">End date for query, inclusive.</param>
+        /// <returns></returns>
+        public async Task<IImmutableList<UserReport>> GetAllUsersByDate(DateTime start, DateTime? end = null)
+        {
+            if (!end.HasValue)
+            {
+                //If not provided, use default pay period length, 14 days.
+                end = start.AddDays(_payPeriodLength);
+            }
+            var query = from u in db.Users
+                select new UserReport
+                {
+                    SlackUserName = u.UserName,
+                    Last = u.LastName,
+                    First = u.FirstName,
+                    BillableHoursYtd = u.TimeEntries
+                        .Where(x=>x.TimeEntryType == TimeEntryTypeEnum.BillableProject && x.Date >= start && x.Date <= end)
+                        .Sum(x=>x.Hours),
+                    OtherNonBillableYtd = u.TimeEntries
+                        .Where(x=>x.TimeEntryType == TimeEntryTypeEnum.NonBillable && x.Date >= start && x.Date <= end)
+                        .Sum(x=>x.Hours),
+                    SickHoursYtd = u.TimeEntries
+                        .Where(x=>x.TimeEntryType == TimeEntryTypeEnum.Sick && x.Date >= start && x.Date <= end)
+                        .Sum(x=>x.Hours),
+                    VacationHoursYtd = u.TimeEntries
+                        .Where(x=>x.TimeEntryType == TimeEntryTypeEnum.Vacation && x.Date >= start && x.Date <= end)
+                        .Sum(x=>x.Hours)
                 };
             return (await query.ToListAsync()).ToImmutableList();
         }
