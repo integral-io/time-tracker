cf504cdcf0aec574c991a0be18ed6d44322c6c34
James Reategui <james@integral.io>
2019-03-29 15:00:35 -0400

    get bundleconfig and bootstrap jqueyr installed

--- time-tracker-webapi/src/TimeTracker.Api/Startup.cs
+++ time-tracker-webapi/src/TimeTracker.Api/Startup.cs
@@ -47,6 +47,8 @@ namespace TimeTracker.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles(); // support wwwroot
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
