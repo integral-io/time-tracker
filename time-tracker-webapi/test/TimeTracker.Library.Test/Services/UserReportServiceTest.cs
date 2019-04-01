0bbefb26703223325f2dd19ff65ebc60491896e9
Hans Buys <hans@integral.io>
2019-04-01 13:37:30 -0400

    Reuse InMemoryDatabase fixture in UserReportService tests.

--- time-tracker-webapi/test/TimeTracker.Library.Test/Services/UserReportServiceTest.cs
+++ time-tracker-webapi/test/TimeTracker.Library.Test/Services/UserReportServiceTest.cs
@@ -9,76 +9,72 @@ using Xunit;
 
 namespace TimeTracker.Library.Test.Services
 {
-    public class UserReportServiceTest
+    public class UserReportServiceTest : IClassFixture<InMemoryDatabaseWithProjectsAndUsers>
     {
+        private readonly Guid userId = Guid.NewGuid();
+        
+        private readonly TimeEntryService entryService;
+        private readonly UserReportService userReportService;
+
+        public UserReportServiceTest(InMemoryDatabaseWithProjectsAndUsers inMemoryDatabase)
+        {
+            entryService = new TimeEntryService(userId, inMemoryDatabase.Database);
+            userReportService = new UserReportService(inMemoryDatabase.Database, userId);
+        }
+
         [Fact]
         public async Task QueryHours_pullsCorrectHoursAndInfo_forUser()
         {
-            var userId = Guid.NewGuid();
-            using (var context = new TimeTrackerDbContext(TestHelpers.BuildInMemoryDatabaseOptions("queryHours1")))
-            {
-                TestHelpers.AddClientAndProject(context);
-                
-                var timeEntryService = new TimeEntryService(userId, context);
-                var dateBefore = new DateTime(2018,11,30);
-                var dateAfter = new DateTime(2019, 1, 15);
-                await timeEntryService.CreateBillableTimeEntry(dateBefore, 7, 1, 1);
-                await timeEntryService.CreateBillableTimeEntry(dateAfter, 7, 1, 1);
+            var dateBefore = new DateTime(2018, 11, 30);
+            var dateAfter = new DateTime(2019, 1, 15);
+            await entryService.CreateBillableTimeEntry(dateBefore, 7, 1, 1);
+            await entryService.CreateBillableTimeEntry(dateAfter, 7, 1, 1);
 
-                var sut = new UserReportService(context, userId);
-                var hours = await sut.QueryAllHours();
+            var hours = await userReportService.QueryAllHours();
 
-                hours.Count.Should().Be(2);
-                hours.Sum(x=>x.Hours).Should().Be(14);
-                hours.First().TimeEntryType.Should().Be(TimeEntryTypeEnum.BillableProject);
-                hours.Select(x => x.ProjectOrName).Should().Contain("au");
-            }
+            hours.Count.Should().Be(2);
+            hours.Sum(x => x.Hours).Should().Be(14);
+            hours.First().TimeEntryType.Should().Be(TimeEntryTypeEnum.BillableProject);
+            hours.Select(x => x.ProjectOrName).Should().Contain("au");
         }
 
         [Fact]
         public async Task GetHoursSummary_summarizesHoursCorrectly()
         {
-            var userId = Guid.NewGuid();
-            using (var context = new TimeTrackerDbContext(TestHelpers.BuildInMemoryDatabaseOptions("hoursReport")))
-            {
-                TestHelpers.AddClientAndProject(context);
-                var currentYear = DateTime.UtcNow.Year;
-                var testMonth = 3;
+            const int testMonth = 3;
 
-                var timeEntryService = new TimeEntryService(userId, context);
-                var dateBefore = new DateTime(2018, 11, 30);
-                var dateAfter = new DateTime(currentYear, testMonth, 2);
-                await timeEntryService.CreateBillableTimeEntry(dateBefore, 7, 1, 1);
-                await timeEntryService.CreateNonBillableTimeEntry(dateBefore.AddDays(-1), 5, null,
-                    TimeEntryTypeEnum.Vacation);
-                
-                await timeEntryService.CreateBillableTimeEntry(dateAfter, 4, 1, 1);
-                await timeEntryService.CreateBillableTimeEntry(dateAfter.AddDays(1), 8, 1, 1);
-                await timeEntryService.CreateBillableTimeEntry(dateAfter.AddDays(2), 8, 1, 1);
-                await timeEntryService.CreateBillableTimeEntry(dateAfter.AddMonths(-1), 8, 1, 1);
-                
-                await timeEntryService.CreateNonBillableTimeEntry(dateAfter.AddDays(15), 5, null,
-                    TimeEntryTypeEnum.Vacation);
-                await timeEntryService.CreateNonBillableTimeEntry(dateAfter.AddDays(16), 6, "dr visit",
-                    TimeEntryTypeEnum.Sick);
-                await timeEntryService.CreateNonBillableTimeEntry(dateAfter.AddDays(17), 7, "PDA",
-                    TimeEntryTypeEnum.NonBillable);
+            var utcNowYear = DateTime.UtcNow.Year;
+            var dateBefore = new DateTime(2018, 11, 30);
+            var dateAfter = new DateTime(utcNowYear, testMonth, 2);
+            await entryService.CreateBillableTimeEntry(dateBefore, 7, 1, 1);
+            await entryService.CreateNonBillableTimeEntry(dateBefore.AddDays(-1), 5, null,
+                TimeEntryTypeEnum.Vacation);
 
-                var sut = new UserReportService(context, userId);
-                var hours = await sut.GetHoursSummaryMonthAndYtd(testMonth);
-                
-                hours.CurrentMonthDisplay.Should().Be("March 2019");
-                hours.BillableHoursMonth.Should().Be(20d);
-                hours.BillableHourssYtd.Should().Be(28d);
-                
-                hours.SickHoursMonth.Should().Be(6d);
-                hours.VacationHoursMonth.Should().Be(5d);
-                hours.NonBillableHoursMonth.Should().Be(7d);
-                
-                hours.SickHoursYtd.Should().Be(6d);
-                hours.VacationHoursYtd.Should().Be(5d);
-                hours.NonBillableHoursYtd.Should().Be(7d);
-            }
+            await entryService.CreateBillableTimeEntry(dateAfter, 4, 1, 1);
+            await entryService.CreateBillableTimeEntry(dateAfter.AddDays(1), 8, 1, 1);
+            await entryService.CreateBillableTimeEntry(dateAfter.AddDays(2), 8, 1, 1);
+            await entryService.CreateBillableTimeEntry(dateAfter.AddMonths(-1), 8, 1, 1);
+
+            await entryService.CreateNonBillableTimeEntry(dateAfter.AddDays(15), 5, null,
+                TimeEntryTypeEnum.Vacation);
+            await entryService.CreateNonBillableTimeEntry(dateAfter.AddDays(16), 6, "dr visit",
+                TimeEntryTypeEnum.Sick);
+            await entryService.CreateNonBillableTimeEntry(dateAfter.AddDays(17), 7, "PDA",
+                TimeEntryTypeEnum.NonBillable);
+
+            var hours = await userReportService.GetHoursSummaryMonthAndYtd(testMonth);
+
+            hours.CurrentMonthDisplay.Should().Be($"March {utcNowYear}");
+            hours.BillableHoursMonth.Should().Be(20d);
+            hours.BillableHourssYtd.Should().Be(28d);
+
+            hours.SickHoursMonth.Should().Be(6d);
+            hours.VacationHoursMonth.Should().Be(5d);
+            hours.NonBillableHoursMonth.Should().Be(7d);
+
+            hours.SickHoursYtd.Should().Be(6d);
+            hours.VacationHoursYtd.Should().Be(5d);
+            hours.NonBillableHoursYtd.Should().Be(7d);
         }
     }
 }
