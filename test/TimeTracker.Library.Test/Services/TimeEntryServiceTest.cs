67a6caaa4b036bc78f44b0921170241689a84c72
ambersalome <ambercsalome@gmail.com>
2019-04-11 13:46:50 -0400

    Cannot add more than 24 hours in a day.

--- test/TimeTracker.Library.Test/Services/TimeEntryServiceTest.cs
+++ test/TimeTracker.Library.Test/Services/TimeEntryServiceTest.cs
@@ -73,6 +73,28 @@ namespace TimeTracker.Library.Test.Services
         }
 
         [Fact]
+        public async Task TheEntriesForADayShouldBeNoGreaterThan24Hours_WhenAddingNonBillableTimeOver()
+        {
+            await timeEntryService.CreateBillableTimeEntry(DateTime.UtcNow, 8, 1, 1);
+            await timeEntryService.CreateNonBillableTimeEntry(DateTime.UtcNow, 8, null, TimeEntryTypeEnum.Vacation);
+
+            await Assert.ThrowsAsync<Exception>(() =>
+                timeEntryService.CreateNonBillableTimeEntry(DateTime.UtcNow, 8.5, "flu", TimeEntryTypeEnum.Sick));
+            try
+            {
+                await timeEntryService.CreateNonBillableTimeEntry(DateTime.UtcNow, 8.5, "flu", TimeEntryTypeEnum.Sick);
+            }
+            catch (Exception e)
+            {
+                Assert.Equal("You may not enter more than 24 hours per day.", e.Message);
+            }
+            
+            var timeEntries = await database.TimeEntries.Where(x => x.UserId == userId).ToListAsync();
+            var hours = timeEntries.Sum(x => x.Hours);
+            hours.Should().Be(16);
+        }
+
+        [Fact]
         public async Task DeleteHours_ThatDontExistFromToday_works()
         {
             var hoursDeleted = await timeEntryService.DeleteHours(DateTime.UtcNow);
