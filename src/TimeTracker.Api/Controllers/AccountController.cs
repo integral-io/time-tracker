b2cd49bd76ae3f9f6c54ab83b67f09d4cec92b25
ambersalome <ambercsalome@gmail.com>
2019-04-24 17:59:47 -0400

    wip link slack and web report

--- src/TimeTracker.Api/Controllers/AccountController.cs
+++ src/TimeTracker.Api/Controllers/AccountController.cs
@@ -1,15 +1,40 @@
+using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TimeTracker.Data;
+using TimeTracker.Library.Services;
 
 namespace TimeTracker.Api.Controllers
 {
-    [Route("ffvfv"), AllowAnonymous]
-    public class AccountController
+    [Route("account"), Authorize]
+    public class AccountController : Controller
     {
-//        public async Task<ViewResult> MyHoursReport(string slackId)
-//        {
-//            
-//        }
+        private readonly TimeTrackerDbContext dbContext;
+        private readonly UserService userService;
+
+        public AccountController(TimeTrackerDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            userService = new UserService(dbContext);
+        }
+
+
+        [HttpGet("linkslack")]
+        public async Task<IActionResult> LinkSlack(string slackUser)
+        {
+            var ident = User.Identity as ClaimsIdentity;
+            var user = ident.Claims.First(x => x.Value == "Identifier");
+            string googleId = user.Value;
+
+            string first = ident.Claims.First(x => x.Value == "First").Value;
+            string last = ident.Claims.First(x => x.Value == "Last").Value;
+            string email = ident.Claims.First(x => x.Value == "Email").Value;
+
+            await userService.SaveGoogleInfo(slackUser, googleId, first, last, email);
+
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
