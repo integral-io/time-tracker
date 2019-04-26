76fbeb87a4b55512bc1a4da084581c7a7b676ce1
ambersalome <ambercsalome@gmail.com>
2019-04-26 14:24:48 -0400

    Rename web classes.

--- src/TimeTracker.Library/Services/Orchestration/WebMessageOrchestration.cs
+++ src/TimeTracker.Library/Services/Orchestration/WebMessageOrchestration.cs
@@ -4,7 +4,7 @@ using TimeTracker.Library.Services.Interpretation;
 
 namespace TimeTracker.Library.Services.Orchestration
 {
-    public class WebMessageOrchestration : MessageOrchestration<WebReportLinkInterpreter, WebReportLinkInterpretedMessage>
+    public class WebMessageOrchestration : MessageOrchestration<WebLinkInterpreter, WebLinkInterpretedMessage>
     {
         private readonly TimeTrackerDbContext dbContext;
         private readonly string webAppUri;
@@ -14,7 +14,7 @@ namespace TimeTracker.Library.Services.Orchestration
             this.dbContext = dbContext;
             this.webAppUri = webAppUri;
         }
-        protected override async Task<SlackMessageResponse> RespondTo(WebReportLinkInterpretedMessage message)
+        protected override async Task<SlackMessageResponse> RespondTo(WebLinkInterpretedMessage message)
         {
             var link = $"{webAppUri}/account/linkslack?slackuser={message.UserId}";
             return new SlackMessageResponse("Click this link " + link + " to access your hours on the web.", true);
