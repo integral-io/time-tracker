ec9865d546fa83fda7f1e63eae9e589a36934e4d
ambersalome <ambercsalome@gmail.com>
2019-05-01 16:26:32 -0400

    Give more info on projects display command.

--- src/TimeTracker.Library/Services/Orchestration/HelpMessageOrchestration.cs
+++ src/TimeTracker.Library/Services/Orchestration/HelpMessageOrchestration.cs
@@ -13,7 +13,7 @@ namespace TimeTracker.Library.Services.Orchestration
             sb.AppendLine("*/hours* record - Allows the user to record hours.");
             sb.AppendLine("*/hours* summary - Shows the users reported hours.");
             sb.AppendLine("*/hours* delete - Deletes hours reported.");
-            sb.AppendLine("*/hours* projects - Lists available projects.");
+            sb.AppendLine("*/hours* projects - Lists available projects (project - client).");
             sb.AppendLine("*/hours* web - Links user to web for hours report.");
             sb.AppendLine("Add 'help' to each one of the options to get specific help. ex: */hours* record help");
 
