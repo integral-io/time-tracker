2ce1ea7fe0a15369f82497565aed669608eac5d6
James Reategui <james@integral.io>
2019-05-03 15:49:32 -0400

    filter all users and hours when filter by project

--- src/TimeTracker.Library/Services/AdminReportService.cs
+++ src/TimeTracker.Library/Services/AdminReportService.cs
@@ -46,7 +46,13 @@ namespace TimeTracker.Library.Services
         /// <returns></returns>
         public async Task<IImmutableList<UserReport>> GetAllUsersByDate(DateTime start, DateTime end, int? projectId = null)
         {
-            var query = from u in db.Users
+            var query = from u in db.Users select u;
+            if (projectId.HasValue)
+            {
+                query = query.Where(x => x.TimeEntries.Any(t => t.ProjectId == projectId));
+            }
+            
+            var projection = from u in query
                 select new UserReport
                 {
                     SlackUserName = u.UserName,
@@ -56,17 +62,20 @@ namespace TimeTracker.Library.Services
                         .Where(x=>x.TimeEntryType == TimeEntryTypeEnum.BillableProject && x.Date >= start && x.Date <= end
                                   && (projectId == null || projectId.HasValue && x.ProjectId == projectId))
                         .Sum(x=>x.Hours),
-                    OtherNonBillableYtd = u.TimeEntries
+                    OtherNonBillableYtd = projectId.HasValue ? 0 : 
+                        u.TimeEntries
                         .Where(x=>x.TimeEntryType == TimeEntryTypeEnum.NonBillable && x.Date >= start && x.Date <= end)
                         .Sum(x=>x.Hours),
-                    SickHoursYtd = u.TimeEntries
+                    SickHoursYtd = projectId.HasValue ? 0 :
+                        u.TimeEntries
                         .Where(x=>x.TimeEntryType == TimeEntryTypeEnum.Sick && x.Date >= start && x.Date <= end)
                         .Sum(x=>x.Hours),
-                    VacationHoursYtd = u.TimeEntries
+                    VacationHoursYtd = projectId.HasValue ? 0 :
+                        u.TimeEntries
                         .Where(x=>x.TimeEntryType == TimeEntryTypeEnum.Vacation && x.Date >= start && x.Date <= end)
                         .Sum(x=>x.Hours)
                 };
-            return (await query.ToListAsync()).ToImmutableList();
+            return (await projection.ToListAsync()).ToImmutableList();
         }
     }
 }
