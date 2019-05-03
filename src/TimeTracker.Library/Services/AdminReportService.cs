5f52b303994615a8e0ee6a0749f673094736445a
James Reategui <james@integral.io>
2019-05-03 16:15:50 -0400

    bug fix and restructure how we build report hours

--- src/TimeTracker.Library/Services/AdminReportService.cs
+++ src/TimeTracker.Library/Services/AdminReportService.cs
@@ -5,6 +5,7 @@ using TimeTracker.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Scaffolding.Internal;
 using TimeTracker.Data.Models;
 using TimeTracker.Library.Models.Admin;
 
@@ -46,36 +47,42 @@ namespace TimeTracker.Library.Services
         /// <returns></returns>
         public async Task<IImmutableList<UserReport>> GetAllUsersByDate(DateTime start, DateTime end, int? projectId = null)
         {
-            var query = from u in db.Users select u;
+            var users = from u in db.Users select u;
             if (projectId.HasValue)
             {
-                query = query.Where(x => x.TimeEntries.Any(t => t.ProjectId == projectId));
+                users = users.Where(x => x.TimeEntries.Any(t => t.ProjectId == projectId));
             }
+
+            var timeEntries = from t in db.TimeEntries
+                where t.Date >= start && t.Date <= end
+                select t;
+            if (projectId.HasValue)
+            {
+                timeEntries = timeEntries.Where(x => x.ProjectId == projectId);
+            }
+
+            var timeEntriesList = await timeEntries.ToListAsync();
             
-            var projection = from u in query
+            var projection = from u in await users.ToListAsync()
                 select new UserReport
                 {
                     SlackUserName = u.UserName,
                     Last = u.LastName,
                     First = u.FirstName,
-                    BillableHoursYtd = u.TimeEntries
-                        .Where(x=>x.TimeEntryType == TimeEntryTypeEnum.BillableProject && x.Date >= start && x.Date <= end
-                                  && (projectId == null || projectId.HasValue && x.ProjectId == projectId))
+                    BillableHoursYtd = timeEntriesList
+                        .Where(x=> x.UserId == u.UserId && x.TimeEntryType == TimeEntryTypeEnum.BillableProject)
                         .Sum(x=>x.Hours),
-                    OtherNonBillableYtd = projectId.HasValue ? 0 : 
-                        u.TimeEntries
-                        .Where(x=>x.TimeEntryType == TimeEntryTypeEnum.NonBillable && x.Date >= start && x.Date <= end)
+                    OtherNonBillableYtd = timeEntriesList
+                        .Where(x=> x.UserId == u.UserId && x.TimeEntryType == TimeEntryTypeEnum.NonBillable)
                         .Sum(x=>x.Hours),
-                    SickHoursYtd = projectId.HasValue ? 0 :
-                        u.TimeEntries
-                        .Where(x=>x.TimeEntryType == TimeEntryTypeEnum.Sick && x.Date >= start && x.Date <= end)
+                    SickHoursYtd = timeEntriesList
+                        .Where(x=> x.UserId == u.UserId && x.TimeEntryType == TimeEntryTypeEnum.Sick)
                         .Sum(x=>x.Hours),
-                    VacationHoursYtd = projectId.HasValue ? 0 :
-                        u.TimeEntries
-                        .Where(x=>x.TimeEntryType == TimeEntryTypeEnum.Vacation && x.Date >= start && x.Date <= end)
+                    VacationHoursYtd = timeEntriesList
+                        .Where(x=> x.UserId == u.UserId && x.TimeEntryType == TimeEntryTypeEnum.Vacation)
                         .Sum(x=>x.Hours)
                 };
-            return (await projection.ToListAsync()).ToImmutableList();
+            return (projection).ToImmutableList();
         }
     }
 }
