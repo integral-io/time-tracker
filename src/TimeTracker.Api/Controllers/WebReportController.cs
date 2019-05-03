597572ea4fecb40c0f492b8d346c6fde6be8656c
James Reategui <james@integral.io>
2019-05-03 15:11:35 -0400

    wip time entry web

--- src/TimeTracker.Api/Controllers/WebReportController.cs
+++ src/TimeTracker.Api/Controllers/WebReportController.cs
@@ -4,7 +4,9 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TimeTracker.Api.Models;
 using TimeTracker.Data;
+using TimeTracker.Data.Models;
 using TimeTracker.Library.Services;
 
 namespace TimeTracker.Api.Controllers
@@ -24,14 +26,39 @@ namespace TimeTracker.Api.Controllers
         [HttpGet("userEntryReport")]
         public async Task<ViewResult> UserEntryReport()
         {
-            var ident = User.Identity as ClaimsIdentity;
-            // refactor this line, also used in AccountController
-            string googleId = ident.Claims.First(x => x.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
-            Guid userId = await userService.GetUserIdFromGoogleId(googleId);
-            
+            var userId = await GetUserId();
+
             var webReportService = new WebReportService(dbContext);
             var items = await webReportService.GetUserReport(userId);
             return View(items);
+        }
+
+        [HttpPost("record")]
+        public async Task<IActionResult> RecordHours(RecordHoursPost model)
+        {
+            var userId = await GetUserId();
+            TimeEntryService timeEntryService = new TimeEntryService(userId, this.dbContext);
+
+            if (model.TimeEntryType == TimeEntryTypeEnum.BillableProject)
+            {
+               // timeEntryService.CreateBillableTimeEntry()
+            }
+            else
+            {
+                
+            }
+
+            return RedirectToAction("UserEntryReport");
+        }
+
+        private async Task<Guid> GetUserId()
+        {
+            var ident = User.Identity as ClaimsIdentity;
+            // refactor this line, also used in AccountController
+            string googleId = ident.Claims
+                .First(x => x.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
+            Guid userId = await userService.GetUserIdFromGoogleId(googleId);
+            return userId;
         }
     }
 }
